787943ba0a75b41a3ff351a4c9c465d3508a51b0
Nicolas Lattemann <6582485n@gmail.com>
2020-08-31 12:10:29 +0200

    edited ReadMe

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bot Jean-Pierre pour le serveur Discord Ada
+
+- suivre le tuto sur https://www.writebots.com/discord-bot-token/ pour <br> la creation du bot et recevoir le token d'authentification 
+
+- intaller les lib discord, os et dotenv
+-
--- botAda.py
+++ botAda.py
@@ -1,14 +1,14 @@
-import os 
+import os
 # importe la librairie os, qui permet d'utiliser certains fonctions déjà adaptées par les systèmes d'exploitation
 
-import discord 
+import discord
 # pouvoir utiliser l'API discord dans notre code
 
 from dotenv import load_dotenv
 # on va chercher dans la librairie dotenv
 # un de ses composants "load_dotenv" qui permet de charger des variables d'env
 
-load_dotenv() 
+load_dotenv()
 # sert à lier le ficher .env au fichier bot
 TOKEN = os.getenv('DISCORD_TOKEN')
 # discord_token => variable, TOKEN => valeur de la variable
